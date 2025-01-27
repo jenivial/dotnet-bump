7e6adca3b8a9da4401fd50931e5fc7e66d2f1aab
Jesus Eduardo Nivia Latorre <chucho@Laptop-de-Jesus.local>
2025-01-27 12:47:02 -0500

    testing

---
+++ DotnetBump/DotnetBump.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DotnetBump.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
